e99afbc7051f8a642c15d6c52f78d533abe9c96e
Harsh Kumar <harsh.23bcs10021@ms.sst.scaler.com>
2024-02-06 17:01:17 +0530

    updated variables and lik button

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let likeCounter=0;
+let likeCounter = 0;
 let input = document.querySelector(".input");
 let submit_btn = document.getElementById("submit-btn");
 let typedText = "";
 let feed_text = document.querySelector(".feed-text");
 let card_area = document.querySelector(".cards-area");
 let x = "";
-let card_content_footer= document.querySelector(".card-content-footer");
+// let card_content_footer= document.querySelector(".card-content-footer");
 let card_content_header = document.querySelector(".card-content-header");
 
+const LIKED_IMAGE = "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/025/original/state_clicked.png?1706888455";
+const UNLIKED_IMAGE = "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/029/original/heart.png?1706888679";
 
 
-submit_btn.addEventListener("click",()=>{
-    typedText=input.value;
-    x=
-    `<div class="card">
+const Post = ({ typedText, src }) => {
+    return `<div class="card">
     <div class="card-icon"><img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/031/original/profile_image.png?1706888739"></div>
     <div class="card-content">
         <div class="card-content-header">
@@ -22,41 +22,30 @@ submit_btn.addEventListener("click",()=>{
             <img class = "delete" src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/027/original/delete.png?1706888643" alt="delete" onclick="deleteCard()">
         </div>
         <div class="feed-text">${typedText}</div>
-        <div class="card-content-footer" onclick="helper()">
+        <div class="card-content-footer" onclick="helper(event)">
             <img class="comment" src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/026/original/comment.png?1706888619" alt="comment">
-            <img class="like" src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/029/original/heart.png?1706888679" alt="like">
+            <img class="like" src="${src}" alt="like" onclick="handleLike(event)">
         </div>
     </div>
     </div>`;
-    card_area.innerHTML=card_area.innerHTML+x;
-    card_content_footer= card_area.querySelector(".card-content-footer");
-    
+}
 
-
+submit_btn.addEventListener("click", () => {
+    x = Post({ typedText: input.value, src: UNLIKED_IMAGE });
+    card_area.innerHTML = card_area.innerHTML + x;
 });
-function helper(){
-card_content_footer.addEventListener("click",(e)=>{
-    if(e.target.classList=="like"){
-        if(likeCounter%2==0){
-            card_content_footer.removeChild(e.target);
-            let like = document.createElement("img");
-            like.setAttribute('src',"https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/025/original/state_clicked.png?1706888455");
-            like.setAttribute("class","like");
-            like.setAttribute("alt","like");
-            likeCounter++;
-            card_content_footer.appendChild(like);
-        }
-        else{
-            card_content_footer.removeChild(e.target);
-            let like = document.createElement("img");
-            like.setAttribute('src',"https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/029/original/heart.png?1706888679");
-            like.setAttribute("class","like");
-            like.setAttribute("alt","like");
-            likeCounter++;
-            card_content_footer.appendChild(like);
-        }
-    }
-})};
-function deleteCard(){
-    
+
+function handleLike(e) {
+    const curElement = e.target;
+
+    const isLike = curElement.getAttribute("alt") == "like";
+    const src = isLike ? LIKED_IMAGE : UNLIKED_IMAGE;
+    const alt = isLike ? "unlike" : "like";
+
+    curElement.setAttribute("src", src);
+    curElement.setAttribute("alt", alt);
+};
+
+function deleteCard() {
+
 }
